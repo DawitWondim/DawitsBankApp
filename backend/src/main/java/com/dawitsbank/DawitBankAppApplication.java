3598914de16ed67dfd267031c50d8e8cc968aaf9
Dawit Wondim <dawit346@gmail.com>
2022-03-11 20:22:00 -0800

    Created my client and backend folders

---
+++ backend/src/main/java/com/dawitsbank/DawitBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.dawitsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawitBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawitBankAppApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/dawitsbank/DawitBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawitsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawitBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
